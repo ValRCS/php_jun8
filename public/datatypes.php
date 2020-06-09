50686db01e4280eb5697cf1cf0c4e896929c5feb
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-09 12:08:37 +0300

    array loops

--- public/datatypes.php
+++ public/datatypes.php
@@ -30,3 +30,13 @@ echo "<hr>";
 $newText = str_replace("aķ", "uŪu", $myStr);
 echo $newText;
 //TODO find string reverse and reverse $newText on screen
+
+echo 'no need to escape here $ works<hr>';
+echo "need to escape \$ here $i gets variable<hr>";
+//multi line string which lets you insert variables inside
+echo <<<MYSTRINGLIM
+    <footer>My name is "{$arr[6]}". I am printing some $i.
+        Now, I am printing some.
+        This should print a capital 'A': \x41
+    </footer>
+MYSTRINGLIM;
