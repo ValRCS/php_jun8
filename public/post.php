1eee1d9915360ef2e37297c1b4af6bd940ec9de8
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-10 11:58:22 +0300

    Login and Logout using Session

--- public/post.php
+++ public/post.php
@@ -1,12 +1,12 @@
 <form action="post.php" method="post">
-<input type="email" name="myEmail" >
-<input type="password" name="myPw" >
-<input type="text" name="myName">
+<input type="email" name="myEmail" placeholder="valdis.s.coding@gmail.com" >
+<input type="password" name="myPw" placeholder="hunter2" required>
+<input type="text" name="myName" placeholder="Valdis" value="Voldemars">
 <button type="submit">Submit Me</button>
 </form>
 <?php
 var_dump($_POST); //another superglobal
-if (isset($_POST['myPw']) && isset($_POST['myName'])) {
+if (isset($_POST['myPw']) && $_POST['myPw'] != "" && isset($_POST['myName'])) {
     $myName = $_POST['myName'];
     $myPw = $_POST['myPw'];
     echo "Going to log in $myName with password $myPw in some place";
