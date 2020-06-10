79b70f0da819b274b0aee199ae7c19f82a8e581e
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-10 12:10:15 +0300

    added comment

--- public/login.php
+++ public/login.php
@@ -12,6 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     echo "Cool got POST method will save my login";
     if (isset($_POST['myName'])) {
         $_SESSION['myName'] = $_POST['myName'];
+        //later we would password checking
         echo "Session saved";
         header("Location: login.php");
     } else {
