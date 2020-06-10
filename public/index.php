85b3013a30567322449843a6e966a454678f4014
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-10 12:18:59 +0300

    clear visit count upon logout

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
+session_start();
+
 require_once "../src/templates/header.php";
 require_once "../src/utilities.php"; //remember no side effects for this file!
 echo "<h1>My PHP page</h1>";
-$arr = ["Valdis", "Pēteris", "Līga"];
-makeUnorderedList($arr);
+
+if (isset($_SESSION['myName'])) {
+    if (!isset($_SESSION['indexVisits'])) {
+        $_SESSION['indexVisits'] = 1;
+    } else {
+        $_SESSION['indexVisits']++;
+    }
+    echo $_SESSION['myName'] . " you have visited this page " . $_SESSION['indexVisits'] . " times<hr>";
+}
+// $arr = ["Valdis", "Pēteris", "Līga"];
+// makeUnorderedList($arr);
 
 echo "<div class='results'>" . myAdder(5, 200) . "</div>";
 echo "<hr>";
--- public/logout.php
+++ public/logout.php
@@ -2,4 +2,5 @@
 session_start();
 // https://www.php.net/manual/en/function.session-unset.php
 unset($_SESSION['myName']);
+unset($_SESSION['indexVisits']);
 header("Location: login.php"); //load this page
