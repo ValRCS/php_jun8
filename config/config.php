8de34586564c24778e7de26836950ede5d12e8c3
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-12 11:23:45 +0300

    Addeed gitignore note

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
+//In Production THIS file should be added to .gitignore
+//even better whole config folder should be added to .gitignore
 $servername = "localhost";
 $username = "root";
 $password = "";
