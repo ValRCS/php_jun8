7a250282f5cef818107c1186b055ca73e7428477
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-15 13:02:39 +0300

    processLogin prepare for registration

--- public/processLogin.php
+++ public/processLogin.php
@@ -1,23 +1,28 @@
 <?php
 session_start();
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    require_once "../config/config.php";
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $stmt = $conn->prepare("SELECT * FROM `users` WHERE username = (?)");
+    if (isset($_POST['login'])) {
+        require_once "../config/config.php";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $stmt = $conn->prepare("SELECT * FROM `users` WHERE username = (?)");
 
-    $stmt->bind_param("s", $_POST["myName"]);
-    $stmt->execute();
-    $result = $stmt->get_result();
+        $stmt->bind_param("s", $_POST["myName"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($result->num_rows == 1) {
-        //cool we found our user
-        $row = $result->fetch_assoc();
-        // var_dump($row);
-        // die("For now");
-        //TODO verify login hash FIXME
-        $_SESSION['user'] = $row['username'];
-        $_SESSION['id'] = $row['id'];
+        if ($result->num_rows == 1) {
+            //cool we found our user
+            $row = $result->fetch_assoc();
+            // var_dump($row);
+            // die("For now");
+            //TODO verify login hash FIXME
+            $_SESSION['user'] = $row['username'];
+            $_SESSION['id'] = $row['id'];
 
+        }
     }
+
+    //TODO check for register button press and register new user
+
 }
 header("Location: /tracks.php"); //always go back to main page
