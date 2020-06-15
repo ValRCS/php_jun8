d8017bac87701c707fc4d3a44c1feafe0b16a189
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-15 10:56:23 +0300

    formatting

--- public/updateSong.php
+++ public/updateSong.php
@@ -1,6 +1,5 @@
 <?php
 if (!isset($_POST['updateSong'])) {
-    // die("You are not adding a song are you?");
     header("Location: /tracks.php"); // we could redirect to error page as well
 }
 require_once "../config/config.php";
@@ -10,10 +9,9 @@ $trackName = $_POST['trackName'];
 $artistName = $_POST['artistName'];
 // var_dump($_POST);
 // die("now");
-//UPDATE `tracks` SET `name` = 'Enter Sandman',
-// `artist` = 'Metallica and friends' WHERE `tracks`.`id` = 12
-$stmt = $conn->prepare("UPDATE `tracks` SET `name` = (?), `artist` = (?)  WHERE `tracks`.`id` = (?)");
+$stmt = $conn->prepare("UPDATE `tracks`
+            SET `name` = (?), `artist` = (?)
+            WHERE `tracks`.`id` = (?)");
 $stmt->bind_param("ssd", $trackName, $artistName, $id); //s means string, d means integer here
 $stmt->execute();
-// echo "Ok should have added song now";
 header("Location: /tracks.php");
