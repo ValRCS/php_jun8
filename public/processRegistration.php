0c8e7173c3dc6a11f820f46603f4458ef5a8c2f4
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-17 10:27:48 +0300

    extra validation for name and pw front and back

--- public/processRegistration.php
+++ public/processRegistration.php
@@ -8,6 +8,16 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         //INSERT INTO `users` (`id`, `username`, `email`, `hash`, `created`)
         // VALUES (NULL, 'Līga', 'ls@gmail.com', 'afdfadfafdaadsfad', current_timestamp())
         //we could check for password length etc and redirect to registration again
+        if (strlen($_POST["myName"]) < 3) {
+            header("Location: /tracks.php?short=3");
+            //we could go a different page also
+            exit();
+        }
+        if (strlen($_POST["pw"]) < 8) {
+            header("Location: /tracks.php?shortpassword");
+            //we could go a different page also
+            exit();
+        }
         $hash = password_hash($_POST['pw'], PASSWORD_DEFAULT);
         $stmt = $conn->prepare("INSERT INTO `users`
             (`username`, `email`, `hash`)
